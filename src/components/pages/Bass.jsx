3ffad12d0028505973580f424248f032cb927571
Mina <124147624+Minanfar@users.noreply.github.com>
2023-11-02 11:25:10 +0100

    Update Bass.jsx

--- src/components/pages/Bass.jsx
+++ src/components/pages/Bass.jsx
@@ -3,7 +3,7 @@ import bass from "./assets/bass.jpg";
 function Bass() {
   return (
     <div>
-      <img src={bass} alt='bass_image' style={{ width: "40rem" }} />
+      <img src={bass} alt='bass_image' style={{ width: "20rem" }} />
     </div>
   );
 }
