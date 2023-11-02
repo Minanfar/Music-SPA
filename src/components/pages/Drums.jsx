216ccdbcefea5a4d5fb753e2a5b6cdfb177ce4af
Mina <124147624+Minanfar@users.noreply.github.com>
2023-11-02 11:25:20 +0100

    Update Drums.jsx

--- src/components/pages/Drums.jsx
+++ src/components/pages/Drums.jsx
@@ -3,7 +3,7 @@ import drum from "./assets/drum.jpg";
 function Drum() {
   return (
     <div>
-      <img src={drum} alt='drum_image' style={{ width: "40rem" }} />
+      <img src={drum} alt='drum_image' style={{ width: "20rem" }} />
     </div>
   );
 }
