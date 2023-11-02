c951bd351b594d11748d14cd88020ed8d0fe18c6
Mina <124147624+Minanfar@users.noreply.github.com>
2023-11-02 11:25:41 +0100

    Update Vocal.jsx

--- src/components/pages/Vocal.jsx
+++ src/components/pages/Vocal.jsx
@@ -3,7 +3,7 @@ import vocal from "./assets/vocal.jpg";
 function Vocal() {
   return (
     <div>
-      <img src={vocal} alt='vocal-image' style={{ width: "40rem" }} />
+      <img src={vocal} alt='vocal-image' style={{ width: "20rem" }} />
     </div>
   );
 }
